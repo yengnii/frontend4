881cdbf854e1e5fe89a15e10e7c5c8bbcd3c2ca8
yengnii <yengni718@gmail.com>
2022-08-10 22:36:36 +0900

    index.html menu create

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+// 로거
+
+const cors = require('cors');
+app.use(cors());
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'views' )))
+app.use(express.static(path.join(__dirname, 'public' )))
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views'), 'index.html');
+})
+app.get('*', (req, res)=> {
+    console.log(req.url);
+    // res.sendFile(path.join(__dirname, 'views'), '404.html');
+    res.send('File not Found');
+})
+app.listen( PORT, ()=> {
+    console.log(`server running on PORT ${PORT}`);
+})
